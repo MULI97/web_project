006ee0d0b72deb67fabbec1b3d06c8f2325a617d
Patience Muli <mulipatience97@gmail.com>
2025-08-01 22:07:31 +0300

    Modified page1.php

--- store/page_1.php
+++ store/page_1.php
@@ -1,8 +1,15 @@
 <?php
-	session_start();
-	
-	$_SESSION['name'] = 'Felix';
-	
-	print $_SESSION['name'];
-	
+session_start();
+
+// Example user data after login (normally comes from database)
+$_SESSION['user_id'] = 'U001';
+$_SESSION['user_name'] = 'Patience Muli';
+$_SESSION['user_email'] = 'patience@example.com';
+$_SESSION['user_role'] = 'citizen'; // roles: citizen, official, admin
+$_SESSION['county'] = 'Nairobi';
+
+// Print a personalized message
+echo "Welcome, {$_SESSION['user_name']}!<br>";
+echo "Role: {$_SESSION['user_role']}<br>";
+echo "County: {$_SESSION['county']}<br>";
 ?>
